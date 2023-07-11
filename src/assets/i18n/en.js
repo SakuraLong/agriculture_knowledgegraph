ae7fe48a554b23fd95452054a07053e243629f5a
边俣 <310734304@qq.com>
2023-07-11 15:53:08 +0800

    i18n 更新

--- src/assets/i18n/en.js
+++ src/assets/i18n/en.js
@@ -1,12 +1,17 @@
 export default {
-    message: {
-        username: "Username",
-        password: "Password",
-        save: "Save",
-        edit: "Edit",
-        update: "Update",
-        delete: "Delete",
-        forever: "Forever",
-        expired: "Expired"
+    views:{
+        othersSubpage:{
+            language:"Language",
+            about:"About",
+            preference:"Preference"
+            
+        },
+        functionSubpage:{
+            entity_recognition:"Entity Recognition",
+            entity_query:"Entity Query",
+            relationship_query:"Relationship Query",
+            agricultural_knowledge_graph:"Agricultural Knowledge Graph",
+            agricultural_knowledge_QA:"Agricultural Knowledge Q&A"
+        },
     }
 };
