2aa581581d3d049aead9b703d1b8a3a43938495d
SakuraLong <1897111@qq.com>
2023-07-20 20:30:44 +0800

    更新文件结构、部分界面参数微调、图片预加载

--- src/views/othersSubpage/components/preferenceSubpage.vue
+++ src/views/othersSubpage/components/preferenceSubpage.vue
@@ -5,14 +5,10 @@
         </div>
         <el-scrollbar class="introduce_container">
             <h1>使用方法</h1>
-            <h4>1. 长按鼠标滚轮</h4>
+            <h4>1. 长按ctrl和~</h4>
             <p>如果操作正确，您将会看到界面上出现圆环菜单栏</p>
             <P>移动鼠标可以进行功能选择</P>
-            <P style="color: red !important;font-weight: 600;">请注意：如果在存在滚动条的组件上长按滚轮</P>
-            <P style="color: red !important;font-weight: 600;">鼠标会改变样式</P>
-            <p style="color: red !important;font-weight: 600;">在松开滚轮后且鼠标离开滚轮区域后恢复</p>
-            <p style="color: red !important;font-weight: 600;">如果没有恢复，请回到存在滚动条存在的区域重新完成上述操作</p>
-            <P>如果有何种方法避免此问题，欢迎联系我们，非常感谢</P>
+            <P>选中之后松开键盘即可进入对应界面</P>
             <h4>2. 当进入子页面时</h4>
             <p>2.1 主页菜单栏进入的子页面通过点击菜单栏按钮切换</p>
             <p>2.2 其余子页面不存在退出按钮时，点击左侧空白区域来进行退出</p>
