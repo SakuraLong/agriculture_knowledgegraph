02d258c3cc52e3c7d21d1f936e78bcaad7c0ac57
nkuAlexLee <1402662448@qq.com>
2023-07-31 20:40:46 +0800

    日期选择器修改

--- src/components/datePicker/datePicker.vue
+++ src/components/datePicker/datePicker.vue
@@ -31,10 +31,12 @@
 </style>
 
 <script>
+import utils from "@/assets/js/utils.js";
 export default {
     data() {
+        const userBorn = utils.getUserMsg().born;
         return {
-            date: Date.now(), // 初始日期为2022年11月15日
+            date: userBorn ? new Date(userBorn) : Date.now(),
         };
     },
     methods: {
