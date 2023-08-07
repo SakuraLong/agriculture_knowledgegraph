12082fcb47903a9b62961fb963d0748626e16653
NKUmianman <1277463846@qq.com>
2023-08-07 17:20:00 +0800

    关系查询界面修改

--- src/views/showerSubpage/components/subpages/mapSubpage.vue
+++ src/views/showerSubpage/components/subpages/mapSubpage.vue
@@ -1,125 +1,142 @@
 <template>
-    <div style="width: 100%; height: 100%; background-color: white">
-        <div class="config_container">
-            <el-switch
-                v-model="is_force"
-                active-text="Force"
-                inactive-text="Graph"
-                :disabled="map_content === ''"
-            />
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <el-switch
-                v-model="is_light"
-                active-text="Light"
-                inactive-text="Dark"
-                :disabled="map_content === ''"
-            />
-        </div>
-        <div class="container" :id="id">
-            <div v-if="no_result">两个实体之间没有关系，或者实体不存在</div>
-        </div>
+  <div style="width: 100%; height: 100%; background-color: white">
+    <div class="config_container">
+      <el-switch
+        v-model="is_force"
+        active-text="Force"
+        inactive-text="Graph"
+        :disabled="map_content === ''"
+      />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <el-switch
+        v-model="is_light"
+        active-text="Light"
+        inactive-text="Dark"
+        :disabled="map_content === ''"
+      />
     </div>
+    <div class="container" :id="id"></div>
+    <div class="prompt_ele" v-if="no_result">
+      两个实体之间没有关系，或者实体不存在
+    </div>
+  </div>
 </template>
 
 <script>
 import Renderer from "@/renderer/renderer";
 export default {
-    data() {
-        return {
-            map_renderer: null,
-            is_light:true,
-            is_force:true,
-            map_content:"",
-            id:"shower_container",
-            no_result:false
-        };
-    },
-    watch:{
-        is_light(){
-            if(this.map_content === "") return;
-            this.map_renderer.dispose();
-            let map_renderer = new Renderer(
-                document.getElementById(this.id),
-                this.map_content,
-                "map"
-            );
-            if(this.is_light){
-                map_renderer.setOption({theme:"light"});
-            }else{
-                map_renderer.setOption({theme:"dark"});
-            }
-            map_renderer.render();
+  data() {
+    return {
+      map_renderer: null,
+      is_light: true,
+      is_force: true,
+      map_content: "",
+      id: "shower_container",
+      no_result: false,
+    };
+  },
+  watch: {
+    is_light() {
+      if (this.map_content === "") return;
+      this.map_renderer.dispose();
+      let map_renderer = new Renderer(
+        document.getElementById(this.id),
+        this.map_content,
+        "map"
+      );
+      if (this.is_light) {
+        map_renderer.setOption({ theme: "light" });
+      } else {
+        map_renderer.setOption({ theme: "dark" });
+      }
+      map_renderer.render();
 
-            this.map_renderer = map_renderer;
-        },
-        is_force(){
-            if(this.map_content === "") return;
-            this.map_renderer.dispose();
-            let map_renderer = new Renderer(
-                document.getElementById(this.id),
-                this.map_content,
-                "map"
-            );
-            if(this.is_force){
-                map_renderer.setOption({layout:"force"});
-            }else{
-                map_renderer.setOption({layout:"graph"});
-            }
-            map_renderer.render();
-
-            this.map_renderer = map_renderer;
-        }
+      this.map_renderer = map_renderer;
     },
-    mounted() {},
-    methods: {
-        renderMapByLink(link_content){
-            try{
-                this.map_renderer.dispose();
-            }catch{
-                //
-            }
-            if(link_content === ""){
-                this.no_result = true;
-            }else{
-                this.no_result = false;
-                let temp =
-                        `- 界面配置
+    is_force() {
+      if (this.map_content === "") return;
+      this.map_renderer.dispose();
+      let map_renderer = new Renderer(
+        document.getElementById(this.id),
+        this.map_content,
+        "map"
+      );
+      if (this.is_force) {
+        map_renderer.setOption({ layout: "force" });
+      } else {
+        map_renderer.setOption({ layout: "graph" });
+      }
+      map_renderer.render();
+
+      this.map_renderer = map_renderer;
+    },
+  },
+  mounted() {},
+  methods: {
+    renderMapByLink(link_content) {
+      try {
+        this.map_renderer.dispose();
+        this.map_renderer = null;
+      } catch {
+        //
+        console.log("销毁错误");
+      }
+      if (link_content === "") {
+        this.map_content = "";
+        this.no_result = true;
+      } else {
+        this.no_result = false;
+        let temp =
+          `- 界面配置
                         - 关系\n` + link_content;
-                this.renderMapByText(temp);
-            }
-        },
-        renderMapByText(map_content) {
-            try{
-                this.map_renderer.dispose();
-            }catch{
-                //
-            }
-            let map_renderer = new Renderer(
-                document.getElementById(this.id),
-                map_content,
-                "map"
-            );
-            map_renderer.render();
-            this.map_content = map_content;
-            this.map_renderer = map_renderer;
-        },
-        setId(id){
-            this.id = id;
-        }
+        this.renderMapByText(temp);
+      }
     },
+    renderMapByText(map_content) {
+      try {
+        this.map_renderer.dispose();
+      } catch {
+        //
+      }
+      let map_renderer = new Renderer(
+        document.getElementById(this.id),
+        map_content,
+        "map"
+      );
+      try {
+        map_renderer.render();
+      } catch {}
+      this.map_content = map_content;
+      this.map_renderer = map_renderer;
+    },
+    setId(id) {
+      this.id = id;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .config_container {
-    width: 100%;
-    height: 25px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .container {
-    width: 100%;
-    height: calc(100% - 25px);
+  position: relative;
+  width: 100%;
+  height: calc(100% - 25px);
+}
+.prompt_ele {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
